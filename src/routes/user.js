951b80f95bbd7d714017bf2232264165fe3ab0a0
Facundo Pascua <fnpascua7@gmail.com>
2023-01-18 17:04:15 -0300

    get a user or all the user in the db

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,10 +3,31 @@ const userSchema = require('../models/user.js');
 
 const router = express.Router();
 
+// create a user
+
 router.post("/user", (req, res) => {
   const user = userSchema(req.body);
   user
     .save()
+    .then(data => res.json(data))
+    .catch(error => res.json({ message: error }))
+});
+
+// get all user
+
+router.get("/user", (req, res) => {
+  userSchema
+    .find()
+    .then(data => res.json(data))
+    .catch(error => res.json({ message: error }))
+});
+
+// get a user
+
+router.get("/user/:id", (req, res) => {
+  const { id } = req.params;
+  userSchema
+    .findById(id)
     .then(data => res.json(data))
     .catch(error => res.json({ message: error }))
 });
